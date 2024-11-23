0153b1cb80c926a2e9fbe2e40b0ae8c8ace84a6b
Ryan <119772970+rjreny@users.noreply.github.com>
2024-11-23 15:20:45 -0700

    Add files via upload
    test

--- Core/Main.java
+++ Core/Main.java
@@ -1,74 +1,56 @@
 package Core;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
-import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-
 import Articles.ArticleDetailsPage;
 import Articles.ArticleSearchPage;
 import Articles.ArticlesListPage;
 import HomePage.*;
 import LoginPage.LoginPage;
-import javafx.scene.layout.Pane;
+import Database.UsersAPI; // Import UsersAPI
+import java.util.concurrent.ExecutionException;
+import java.util.List;
+import Database.Models.*;
 
 public class Main extends Application {
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	public void start(Stage baseStage) {
-		Navigation.setPrimaryStage(baseStage);
-		
-		LoginPage.RegisterWithNavigation();
-		
-		//UserHomePage.RegisterWithNavigation();
-		//InstructorHomePage.RegisterWithNavigation();
-		
-		
-		try {
-			ArticlesListPage.RegisterWithNavigation();
-		} catch (InterruptedException e) {
 
-		} catch (ExecutionException e) {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-		}
+    @Override
+    public void start(Stage baseStage) {
+        Navigation.setPrimaryStage(baseStage);
+
+        // Register pages
+        LoginPage.RegisterWithNavigation();
+
+        // Fetch users from the database
+        List<User> users = UsersAPI.getAllUsers();
+        ManageUsersPage.RegisterWithNavigation(users); // Pass the list of users
+
+        // Register other pages...
+        try {
+            ArticlesListPage.RegisterWithNavigation();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         ArticleDetailsPage.RegisterWithNavigation();
         try {
-			ArticleSearchPage.RegisterWithNavigation();
-		} catch (InterruptedException e) {
+            ArticleSearchPage.RegisterWithNavigation("Welcome");
+        } catch (Exception e) { // Catch only relevant exceptions or remove the block if unnecessary
+            e.printStackTrace();
+        }
 
-		} catch (ExecutionException e) {
 
-		}
-        
         ManageArticlesPage.RegisterWithNavigation();
         ManageGroupsPage.RegisterWithNavigation();
-        ManageUsersPage.RegisterWithNavigation();
-        AdminHomePage.RegisterWithNavigation();
-        InstructorHomePage.RegisterWithNavigation();
-        
-		
-		
-	    //AccountCreationUI.RegisterWithNavigation(baseStage);
-		
-        Navigation.navigateTo("ManageArticlesPage");
+        InstructorHomePage.RegisterWithNavigation(null); // Pass null for now, user will be passed on login
+        AdminPanel.RegisterWithNavigation();
+
+        // Navigate to the LoginPage first
+        Navigation.navigateTo("LoginPage");
     }
 }
+
+
